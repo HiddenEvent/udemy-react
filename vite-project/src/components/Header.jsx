8a860f305a05a5042b174937c16f37a2350e12c6
RichardKim <hiddeneventor@gmail.com>
2024-09-19 19:32:54 +0900

    기타 리스트 컴포넌트 분리

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,4 +1,11 @@
+import React from 'react';
+
 export default function Header() {
   const total = 100;
-  return <p>Total a pagar: {total}</p>;
+  return (
+    <>
+      <p>Total a pagar: {total}</p>
+      <div>div 테그 추가</div>
+    </>
+  );
 }
